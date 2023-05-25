64d29c90b0ba2e3bc0a96be273907ba0cdb1a0d1
danys <danys@PC-di-Daniela.Home>
2023-05-25 16:20:38 +0200

    fine

--- src/main/java/it/polito/tdp/itunes/model/Album.java
+++ src/main/java/it/polito/tdp/itunes/model/Album.java
@@ -1,13 +1,29 @@
 package it.polito.tdp.itunes.model;
 
-public class Album {
+public class Album implements Comparable<Album>{
 	private Integer albumId;
 	private String title;
+	private Integer numSongs;
 	
-	public Album(Integer albumId, String title) {
+	public Album(Integer albumId, String title, Integer numSongs) {
 		super();
 		this.albumId = albumId;
 		this.title = title;
+		this.numSongs = numSongs;
+	}
+	
+	/**
+	 * @return the numSongs
+	 */
+	public Integer getNumSongs() {
+		return numSongs;
+	}
+
+	/**
+	 * @param numSongs the numSongs to set
+	 */
+	public void setNumSongs(Integer numSongs) {
+		this.numSongs = numSongs;
 	}
 
 	public Integer getAlbumId() {
@@ -54,6 +70,11 @@ public class Album {
 	@Override
 	public String toString() {
 		return title;
+	}
+
+	@Override
+	public int compareTo(Album o) {
+		return title.compareTo(o.getTitle());
 	}
 	
 	
